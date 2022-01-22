e8305170a879898f48c50089faf638b300b78829
Yazmin Perez <zoeperez333@gmail.com>
2022-01-22 11:20:42 -0800

    WIP using .toggle() to hide/show img, p for each animal. All imgs, p are opening/closing when one hits any of the names.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
 $(document).on("change",".switch",function() {
   $("body").toggleClass("dark");
 });
+
+$(document).ready(function() {
+  $(".clickable-b, .clickable-w, .clickable-r").click(function() {
+    $("#toggle-bear").toggle();
+    $("#toggle-wolf").toggle();
+    $("#toggle-raven").toggle();
+     });
+    });
